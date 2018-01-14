de1f7f4a661f585abada048309cd99e8d09071ab
DESKTOP\sitef <siteforger@gmail.com>
2018-01-14 17:27:14 -0600

    commit001

---
+++ BattleTank001/Source/BattleTank001/BattleTank001.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank001.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleTank001, "BattleTank001" );
